bf9db268e54a3b6db04d028678bfb2150e5f5e87
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2022-01-19 00:06:56 -0500

    update

--- server.js
+++ server.js
@@ -2,7 +2,34 @@ const inquirer = require('inquirer');
 const mysql2 = require('mysql2');
 const tablePrint = require('console.table');
 
-console.table([
+let viewAllDepartments = () => {
+  let sql = "SELECT * FROM departemnts";
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+}
+
+inquirer
+  .prompt({
+    type: 'list',
+    name: 'choice',
+    message: 'What would you like to do?',
+    choices: ['View All Departments', 'View All Roles','View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update Employee Role']
+  }).then(({ choice }) => {
+    console.log(choice);
+    if(choice == 'View All Departments') {
+      viewAllDepartments();
+    }
+  });
+
+/*console.table([
   {
     name: 'foo',
     age: 10
@@ -10,4 +37,15 @@ console.table([
     name: 'bar',
     age: 20
   }
-]);
+]);*/
+
+/*db.query(sql, (err, rows) => {
+  if (err) {
+    res.status(500).json({ error: err.message });
+    return;
+  }
+  res.json({
+    message: "success",
+    data: rows,
+  });
+});*/
---
+++ utils/read.js
@@ -0,0 +1,13 @@
+let viewAllDepartments = () => {
+  let sql = "SELECT * FROM departemnts";
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+}
